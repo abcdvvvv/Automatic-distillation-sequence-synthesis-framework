bb6fc981f381914923c96bf246733719f10888c9
karei <56168406+abcdvvvv@users.noreply.github.com>
2023-08-22 12:11:18 +0900

    Delete function directory

--- function/evoke.m
+++
@@ -1,7 +0,0 @@
-function evoke(mydir,filename)
-global aspen
-aspen = actxserver('apwn.document'); % 38.0(V12)
-aspen.InitFromArchive2([mydir,filename]);
-aspen.Visible = 1;
-aspen.SuppressDialogs = 1;
-end
