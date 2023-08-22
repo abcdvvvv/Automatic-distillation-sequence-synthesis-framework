12b4676a3aae1169941ae50411882deb706140b5
karei <56168406+abcdvvvv@users.noreply.github.com>
2023-08-22 20:01:48 +0900

    Add files via upload

--- function/DSadjust.m
+++ function/DSadjust.m
@@ -1,71 +1,76 @@
-function DSadjust(allcol,gen_rule,TCMX,PBUB,PDEW)
+function DSadjust(allcol,gen_rule,colpressure,TCMX,PBUB,PDEW)
 global aspen column_num AF columnio
 block = aspen.Tree.FindNode('\Data\Blocks');
 disp('【Adjust DSTWU DS】')
 
 %% Adjust column pressure
-% skip=zeros(column_num,1);
-% cost1=zeros(column_num,1);
-% cost2=zeros(column_num,1);
-% for i=1:column_num
-%     if allcol(i).PTOP == gen_rule{1}(1).P && columnio{i,7}==0
-%         P(i)=gen_rule{1}(1).P;
-%         if PBUB(i)>P(i)
-%             cost1(i)=sum(fOPEX(allcol,i))+sum(fCAPEX(allcol,i))*AF;
-%             if PBUB(i)<14.8
-%                 % fprintf('  set pressure to %.2f bar\n',PBUB(i));
-%                 P(i)=PBUB(i);
-%             else
-%                 fprintf('  use parital condenser\n');
-%                 block.FindNode(['T',num2str(i),'\Input\OPT_RDV']).value = 'VAPOR';
-%                 if PDEW(i)<25.2
-%                     % fprintf('  set pressure to %.2f bar\n',PDEW(i));
-%                     P(i)=PDEW(i);
-%                 else
-%                     fprintf('Pressure exceeds 2.52MPa, please consider using refrigerant. Pressure is set to 2.86MPa.\n');
-%                     P(i)=28.6;
-%                 end
-%             end
-%             if P(i)<1
-%                 fprintf('  reset pressure to 1 bar\n');
-%                 P(i)=1;
-%             end
-%         end
-%     else
-%         P(i)=allcol(i).PTOP;
-%         skip(i)=1;
-%     end
-% end
-% [T_cond,T_reb]=temp(P,1:column_num);
-% allcol = readcolumn();
-% cost2(skip==0)=fOPEX(allcol,find(skip==0))+fCAPEX(allcol,find(skip==0))*AF;
-% fprintf('T%d ',find(cost2>cost1));
-% fprintf('have a higher TAC after changing pressure, back to the original pressure=%.1f bar',gen_rule{1}(1).P);
-% P(cost2>cost1)=gen_rule{1}(1).P;
-% skip(cost2>cost1)=1;
-% 
-% for i=1:column_num
-%     if skip(i)==0 && columnio{i,7}==0
-%         if T_reb(i)>350 || T_reb(i)>TCMX(i)
-%             fprintf('Bottom temperature exceeds the decomposition temperature 350°C or critical temperature %.0f°C.\n',TCMX(i));
-%             while T_reb(i)>350 || T_reb(i)>TCMX(i)
-%                 P(i)=P(i)-0.1;
-%                 [T_cond(i),T_reb(i)]=temp(P(i),i);
-%             end
-%             if P(i)<1
-%                 if T_cond(i)>=40
-%                     fprintf('Using vacuum operation, P=%.2f bar\n',P(i));
-%                 else 
-%                     fprintf('Vacuum column with top temperature less than 40°C, extraction or absorption is recommended instead of distillation,\n');
-%                     fprintf('Reset pressure to default value %.2f .\n',gen_rule{1}(1).P);
-%                     P(i)=gen_rule{1}(1).P;
-%                     temp(P(i),i);
-%                 end
-%             end
-%         end
-%         fprintf('T%d final pressure=%.2f\n',i,P(i));
-%     end
-% end
+if colpressure
+skip=zeros(column_num,1);
+cost1=zeros(column_num,1);
+cost2=zeros(column_num,1);
+for i=1:column_num
+    if allcol(i).PTOP == gen_rule{1}(1).P && columnio{i,7}==0
+        P(i)=gen_rule{1}(1).P;
+        if PBUB(i)>P(i)
+            cost1(i)=sum(fOPEX(allcol,i))+sum(fCAPEX(allcol,i))*AF;
+            if PBUB(i)<14.8
+                % fprintf('  set pressure to %.2f bar\n',PBUB(i));
+                P(i)=PBUB(i);
+            else
+                fprintf('  use parital condenser\n');
+                block.FindNode(['T',num2str(i),'\Input\OPT_RDV']).value = 'VAPOR';
+                if PDEW(i)<25.2
+                    % fprintf('  set pressure to %.2f bar\n',PDEW(i));
+                    P(i)=PDEW(i);
+                else
+                    fprintf(['Pressure exceeds 2.52MPa, please consider using refrigerant. ' ...
+                        'Pressure is set to 2.86MPa.\n']);
+                    P(i)=28.6;
+                end
+            end
+            if P(i)<1
+                fprintf('  reset pressure to 1 bar\n');
+                P(i)=1;
+            end
+        end
+    else
+        P(i)=allcol(i).PTOP;
+        skip(i)=1;
+    end
+end
+[T_cond,T_reb]=temp(P,1:column_num);
+allcol = readcolumn();
+cost2(skip==0)=fOPEX(allcol,find(skip==0))+fCAPEX(allcol,find(skip==0))*AF;
+fprintf('T%d ',find(cost2>cost1));
+fprintf('have a higher TAC after changing pressure, back to the original pressure=%.1f bar',gen_rule{1}(1).P);
+P(cost2>cost1)=gen_rule{1}(1).P;
+skip(cost2>cost1)=1;
+
+for i=1:column_num
+    if skip(i)==0 && columnio{i,7}==0
+        if T_reb(i)>350 || T_reb(i)>TCMX(i)
+            fprintf(['Bottom temperature exceeds the decomposition temperature 350°C or ' ...
+                'critical temperature %.0f°C.\n'],TCMX(i));
+            while T_reb(i)>350 || T_reb(i)>TCMX(i)
+                P(i)=P(i)-0.1;
+                [T_cond(i),T_reb(i)]=temp(P(i),i);
+            end
+            if P(i)<1
+                if T_cond(i)>=40
+                    fprintf('Using vacuum operation, P=%.2f bar\n',P(i));
+                else 
+                    fprintf(['Vacuum column with top temperature less than 40°C, extraction or ' ...
+                        'absorption is recommended instead of distillation,\n']);
+                    fprintf('Reset pressure to default value %.2f .\n',gen_rule{1}(1).P);
+                    P(i)=gen_rule{1}(1).P;
+                    temp(P(i),i);
+                end
+            end
+        end
+        fprintf('T%d final pressure=%.2f\n',i,P(i));
+    end
+end
+end
 
 %% Adjust design specifications
 t=1;
