785cfbe7e93d4b3f1145da32ce5c40d6f8a1412e
karei <56168406+abcdvvvv@users.noreply.github.com>
2024-08-01 23:17:32 +0900

    Add files via upload

--- function/modify_param.m
+++ function/modify_param.m
@@ -1,17 +1,18 @@
 function modify_param()
-global aspen column_num
-disp('【Optimize design parameters】')
-disp('(1)stage vs RR')
+global aspen column_num columnio
+disp('[Optimize design parameters]')
+disp('(1) stage vs RR')
 block = aspen.Tree.FindNode('\Data\Blocks\');
 
 for i = 1:column_num
+    if columnio{i,7}~=0, continue, end
     min_reflux(i) = block.FindNode(['T', num2str(i), '\Output\MIN_REFLUX']).value;
     min_stages(i) = block.FindNode(['T', num2str(i), '\Output\MIN_STAGES']).value;
     block.FindNode(['T', num2str(i), '\Input\PLOT']).value = 'YES';
     block.FindNode(['T', num2str(i), '\Input\OPT_SZNO']).value = 'SIZE';
     block.FindNode(['T', num2str(i), '\Input\INCR']).value = 1;
     block.FindNode(['T', num2str(i), '\Input\LOWER']).value = round(min_stages(i)) + 1;
-    block.FindNode(['T', num2str(i), '\Input\UPPER']).value = 97;
+    block.FindNode(['T', num2str(i), '\Input\UPPER']).value = 100;
 end
 aspen.Reinit;
 run();
@@ -19,6 +20,7 @@ pause(1)
 RR = [];
 stage = [];
 for i = 1:column_num
+    if columnio{i,7}~=0, continue, end
     for j = round(min_stages(i)) + 1:97
         stage(i, j-round(min_stages(i))) = j;
         RR(i, j-round(min_stages(i))) = block.FindNode(['T', num2str(i), '\Output\RR_OUT\', num2str(j)]).value;
@@ -26,9 +28,10 @@ for i = 1:column_num
 end
 RR_stage = RR .* stage;
 RR_stage(RR_stage == 0) = inf;
-disp('(2)apply optimized parameters')
+disp('(2) apply optimized parameters')
 [~, p] = min(RR_stage,[],2);
 for i = 1:column_num
+    if columnio{i,7}~=0, continue, end
     % plot(stage(i,:),RR(i,:))
     block.FindNode(['T',num2str(i),'\Input\OPT_NTRR']).value = 'NSTAGE';
     block.FindNode(['T',num2str(i),'\Input\NSTAGE']).value = stage(i, p(i));
