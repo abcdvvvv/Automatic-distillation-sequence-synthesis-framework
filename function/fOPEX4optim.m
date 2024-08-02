86c298a32e894507937a7b46662e389a1a7e8a64
karei <56168406+abcdvvvv@users.noreply.github.com>
2024-08-02 10:54:10 +0900

    Add files via upload

--- function/fOPEX4optim.m
+++ function/fOPEX4optim.m
@@ -1,40 +1,58 @@
-function OPEX = fOPEX4optim(F,optim_col)
+function OPEX = fOPEX4optim(F,allcol,QW,QS,exheat_num)
 global mydir column_num
-[cost_CW,cost_CHW,cost_BW,cost_S,cost_LPS,cost_MPS,cost_HPS,cost_SHPS] = utilities();
-K1 = readmatrix([mydir,'output.xlsx'],'Range',['G2:G',num2str(column_num+1)]);
-K2 = readmatrix([mydir,'output.xlsx'],'Range',['H2:H',num2str(column_num+1)]);
+[cost_CW,cost_CHW,cost_BW,cost_S,cost_LPS,cost_MPS,cost_HPS,cost_SHPS,~] = utilities();
+if nargin<=2
+    K = readmatrix([mydir,'output.xlsx'],'Range',['G2:H',num2str(column_num+1)]);
+end
 
 total_cooling = 0;
 total_heating = 0;
 for i = 1:column_num
     % Cooling Cost
-    T_cond = optim_col.TTOP;
-    if T_cond > 40
-        price = cost_CW;
-    elseif T_cond > 15 && T_cond <= 40
-        price = cost_CHW;
-    elseif T_cond > -5 && T_cond <= 15
-        price = cost_BW;
-    elseif T_cond <= -5
-        price = cost_BW;
+    T_cond=allcol(i).TTOP;
+    if T_cond>40
+        unit_cooling=cost_CW;
+    elseif T_cond>15
+        unit_cooling=cost_CHW;
+    elseif T_cond>-5
+        unit_cooling=cost_BW;
+    elseif T_cond<=-5
+        unit_cooling=cost_BW;
+        fprintf('Warning: Lack of refrigerant prices\n')
     end
-    total_cooling = total_cooling+price*abs(K1(i))*F(i)*8000;
-    % Heating Cost
-    T_reb = optim_col.TBOT;
-    if T_reb < 115
-        price = cost_S;
-    elseif T_reb >= 115 && T_reb < 155
-        price = cost_LPS;
-    elseif T_reb >= 155 && T_reb < 179
-        price = cost_MPS;
-    elseif T_reb >= 179 && T_reb < 249
-        price = cost_HPS;
-    elseif T_reb >= 249
-        price = cost_SHPS;
+    if nargin<=2
+        total_cooling = total_cooling + unit_cooling * abs(K(i,1)) * F(i) * 8000;
+    else
+        total_cooling = total_cooling + unit_cooling * -QW(i) * 3600 * 8000 / 1e6;
     end
-    total_heating = total_heating+price*K2(i)*F(i)*8000;
-end
-% Operating Expense
-OPEX = total_cooling+total_heating;
 
+    % Heating Cost
+    T_reb=allcol(i).TBOT;
+    if T_reb<115.21
+        unit_heating=cost_S;
+    elseif T_reb<153.83
+        unit_heating=cost_LPS;
+    elseif T_reb<179.06
+        unit_heating=cost_MPS;
+    elseif T_reb<248.26
+        unit_heating=cost_HPS;
+    else
+        unit_heating=cost_SHPS;
+    end
+    if nargin<=2
+        total_heating = total_heating + unit_heating * K(i,2) * F(i) * 8000;
+    else
+        total_heating = total_heating + unit_heating * QS(i) * 3600 * 8000 / 1e6;
+    end
+end
+
+if nargin>2 && exheat_num>0
+    for i=1:exheat_num
+        exCost(i) = (-QW(column_num+i)*cost_CW+QS(column_num+i)*cost_S) * 3600 * 8000 / 10^6;
+    end
+else
+    exCost=0;
+end
+
+OPEX = total_cooling + total_heating + sum(exCost);
 end
